8cf444e566a80a402649815211428fe80112f508
Konstantinos Christofilos <kostas.christofilos@rocketgraph.com>
2015-08-03 17:17:24 +0300

    Unit tests

---
+++ tests/reports-lib/Connectors/StripeConnectorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of Rocketgraph service
+ * <http://www.rocketgraph.com>.
+ */
+
+namespace Tests\RAM\Connectors;
+
+use RAM\Connectors\StripeConnector;
+
+/**
+ * Description of StripeConnectorTest.
+ *
+ * @author K.Christofilos <kostas.christofilos@rocketgraph.com>
+ */
+class StripeConnectorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $connector;
+
+    protected $response;
+
+    protected $authUrl;
+
+    public function setUp()
+    {
+        $browser = $this->getMockBuilder('Buzz\Browser')
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'get'
+            ])
+            ->getMock();
+
+        $response = $this->getMockBuilder('Guzzle\Http\Message\Response')
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'getContent'
+            ])
+            ->getMock();
+        $response->expects($this->any())
+            ->method('getContent')
+            ->willReturnCallback(function() {
+                return $this->response;
+            });
+
+        $browser->expects($this->any())
+            ->method('get')
+            ->willReturn($response);
+
+        $this->connector = $this->getMockBuilder('RAM\Connectors\StripeConnector')
+            ->setConstructorArgs([$browser])
+            ->setMethods([
+                'getAuthorizationUrl'
+            ])
+            ->getMock();
+
+        $this->connector->expects($this->any())
+            ->method('get')
+            ->willReturnCallback(function() {
+                return $this->response;
+            });
+
+        $this->connector->expects($this->any())
+            ->method('getAuthorizationUrl')
+            ->willReturnCallback(function() {
+                return $this->authUrl;
+            });
+    }
+
+    public function testConnector()
+    {
+        $path = $this->connector->buildUrlFromPath('path');
+        $this->assertEquals(StripeConnector::API_HOST.'/'.StripeConnector::API_VERSION.'/path', $path);
+
+        $this->assertEquals('', $this->connector->getDisplayName());
+        $response = new \stdClass();
+        $response->display_name = 'Name';
+
+        $this->response = json_encode($response);
+        $this->assertEquals('Name', $this->connector->getDisplayName());
+
+        $this->assertEquals($response, $this->connector->get('path', ['option']));
+        $this->authUrl = 'http://rocketgraph.com';
+        $this->assertEquals('http://rocketgraph.com&display=popup', $this->connector->retrieveAuthUrl());
+
+        $this->assertEquals('https://connect.stripe.com/oauth/authorize', $this->connector->urlAuthorize());
+        $this->assertEquals('https://connect.stripe.com/oauth/token', $this->connector->urlAccessToken());
+    }
+}
---
+++ tests/reports-lib/Connectors/TwitterConnectorTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of Rocketgraph service
+ * <http://www.rocketgraph.com>
+ */
+
+namespace Tests\RAM\Connectors;
+use RAM\Connectors\TwitterConnector;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of TwitterConnectorTest
+ *
+ * @author K.Christofilos <kostas.christofilos@rocketgraph.com>
+ */
+class TwitterConnectorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $connector;
+
+    protected $response;
+
+    protected $authUrl;
+
+    public function setUp()
+    {
+        $browser = $this->getMockBuilder('Buzz\Browser')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->connector = $this->getMockBuilder('RAM\Connectors\TwitterConnector')
+            ->setConstructorArgs([$browser])
+            ->setMethods([
+                'getAuthorizationUrl',
+                'get'
+            ])
+            ->getMock();
+
+        $this->connector->expects($this->any())
+            ->method('get')
+            ->willReturnCallback(function() {
+                return $this->response;
+            });
+
+        $this->connector->expects($this->any())
+            ->method('getAuthorizationUrl')
+            ->willReturnCallback(function() {
+                return $this->authUrl;
+            });
+    }
+
+    public function testConnector()
+    {
+        $path = $this->connector->buildUrlFromPath('path');
+        $this->assertEquals(TwitterConnector::API_HOST.'/'.TwitterConnector::API_VERSION.'/path.json', $path);
+
+        $this->assertEquals('', $this->connector->getDisplayName());
+        $response = new \stdClass();
+        $response->screen_name = 'Name';
+
+        $this->response = $response;
+        $this->assertEquals('Name', $this->connector->getDisplayName());
+
+        $this->assertEquals($response, $this->connector->get('path', ['option']));
+
+        $this->assertEquals('https://api.twitter.com/oauth/authenticate', $this->connector->urlAuthorization());
+        $this->assertEquals('https://api.twitter.com/oauth/access_token', $this->connector->urlTokenCredentials());
+        $this->assertEquals('https://api.twitter.com/1.1/account/verify_credentials.json', $this->connector->urlUserDetails());
+        $this->assertEquals('https://api.twitter.com/oauth/request_token', $this->connector->urlTemporaryCredentials());
+
+        $request = new Request();
+        $this->assertFalse($this->connector->isResponse($request));
+        $request->query->set('oauth_token', 'token');
+        $request->query->set('oauth_verifier', 'verifier');
+        $this->assertTrue($this->connector->isResponse($request));
+    }
+}
