2bf4eb65ceff9dd20a291a380e2fbbbb7d6871fa
Konstantinos Christofilos <kostasxx@gmail.com>
2015-09-11 21:10:00 +0300

    Soundcloud connector

---
+++ reports-lib/Connectors/Mock/SoundcloudMockConnector.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Rocketgraph service
+ * <http://www.rocketgraph.com>
+ */
+
+namespace RAM\Connectors\Mock;
+use RAM\Connectors\SoundcloudConnector;
+use RG\Traits\MockConnectorTrait;
+
+/**
+ * Description of SoundcloudMockConnector
+ *
+ * @author K.Christofilos <kostas.christofilos@rocketgraph.com>
+ */
+class SoundcloudMockConnector extends SoundcloudConnector
+{
+    use MockConnectorTrait;
+}
